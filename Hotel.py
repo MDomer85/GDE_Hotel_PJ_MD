c0be1935c12b9613a104ed01a3fa47bba6f92d1e
Martinecz Dominik <Martdom85@gmail.com>
2024-04-26 16:17:33 +0200

    booking  added

--- Hotel.py
+++ Hotel.py
@@ -123,8 +123,157 @@ def factory():
                 temproomList = validatedReservation()
                         
                 reservationList.append(reservation(reservNumCount, tempnameList[0], tempnameList[1], temproomList[0] , dateGenerator() , temproomList[1])) #innen folyt kov ranndom.randit csere ellenorzesre h van e szoba szam
-#endregion         
+#endregion
+         
+#region Booking
+def booking():
+        from datetime import datetime
+        from datetime import date
+        def refresh():
+                banner()
+                print("{:<80} {:<105} {:<3}".format(" | ","Foglalást választotta"," | "))
+                print (" |", "-"*184, "|")
+        
+        #region Validation
+        def validateName():
+                temp = input()
+                return temp
+        
+        def validateEmail():
+                try:
+                        temp = input()
+                        if "@" not in temp:
+                                return False, temp
+                        else:
+                                return True, temp
+                except:
+                        return False, temp
+                
+        def validateRoomNumber():
+                try:
+                        temp = int(input())
+                        availableRooms = len(roomsList)
+                        if 1 <= temp <= availableRooms:
+                                return True, temp
+                        else:
+                                return False, availableRooms
+                except:
+                        return False, availableRooms
+        
+        def validateDate(wantedRoom):
 
+                temp = input()
+                try:
+                        datetime.strptime(temp, "%Y-%m-%d")
+                        if datetime.now().strftime('%Y-%m-%d') > temp:
+                                return False, "pasterror"
+                        for item in reservationList:
+                                if item.roomNumber == wantedRoom and temp == item.date:
+                                        return False, temp
+                        return True, temp
+                except ValueError:
+                       return False, "formaterror"
+        #endregion       
+        
+        def savebooking():
+                price = 0
+                for item in roomsList:
+                        if book[2] == item.roomNumber:
+                                price = item.price
+                id = int(len(reservationList))
+                # print(book)
+                reservationList.append(reservation(id+1,book[0],book[1],book[2],book[3],price))
+                print(" |", " "*184, "|")
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Sikeres szoba foglalás!"," | "))
+                print(" |", "-"*184, "|")
+                print("{:<58} {:<127} {:<3}".format(" | ",f"A {book[2]}. Szoba {book[3]}-ai dátummal, {book[0]}-néven lefoglalva"," | "))
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Fizetendő összeg: {price} $"," | "))
+                input()
+                
+        book = []
+        refresh()
+#region Nightmare
+        def lvl1():
+                refresh()
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Név: {book[0]} "," | "))
+        def lvl2():
+                lvl1()
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Email: {book[1]} "," | "))
+        def lvl3():
+                lvl2()
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Szoba szám: {book[2]} "," | "))
+        def lvl4():
+                lvl3()
+                print("{:<80} {:<105} {:<3}".format(" | ",f"Dátum: {book[3]} "," | "))
+#endregion
+
+        while True: 
+                if len(book) != 0:
+                        lvl1()
+                        if len(book) != 1:
+                                lvl2()
+                                if len(book) != 2:
+                                        lvl3()
+                                        if len(book) !=3:
+                                                lvl4()
+                                                savebooking()
+                                                break
+                                        else:
+                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Dátum: [(YYYY-MM-DD)] "," | "))
+                                                array = validateDate(book[2])
+                                                while True:
+                                                        if array[0] is True:
+                                                                book.append(array[1])
+                                                                break
+                                                        elif array[0] is False and array[1] == "formaterror":
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"A megadott dátum helytelen! (YYYY-MM-DD)-formátumba adja meg!"," | "))
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Nyomj entert az újrapróbáláshoz! "," | "))
+                                                                input()
+                                                                break 
+                                                        elif array[0] is False and array[1] == "pasterror":
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"A megadott dátum helytelen (Múltbeli)"," | "))
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Nyomj entert az újrapróbáláshoz! "," | "))
+                                                                input()
+                                                                break
+                                                        else:
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"A {book[2]}. szoba már foglalt {array[1]} dátumon! "," | "))
+                                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Nyomj entert az újrapróbáláshoz! "," | "))
+                                                                input()
+                                                                break
+                                                
+                                else:
+                                        print("{:<80} {:<105} {:<3}".format(" | ",f"Szoba szám: [____________] "," | "))
+                                        array = validateRoomNumber()
+                                        while True:
+                                                if array[0] is True:
+                                                        book.append(array[1])
+                                                        break
+                                                else:
+                                                        print("{:<80} {:<105} {:<3}".format(" | ",f"Hibás Szobaszám (1-{array[1]}.ig válasszon) "," | "))
+                                                        print("{:<80} {:<105} {:<3}".format(" | ",f"Nyomj entert az újrapróbáláshoz! "," | "))
+                                                        input()
+                                                        break
+                                
+                        else: 
+                                print("{:<80} {:<105} {:<3}".format(" | ",f"Email: [____________] "," | "))
+                                array = validateEmail()
+                                while True:
+                                        if array[0] is True:
+                                                book.append(array[1])
+                                                break
+                                        else:
+                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Hibás Formátum! (@-ot tartalmaznia kell!) "," | "))
+                                                print("{:<80} {:<105} {:<3}".format(" | ",f"Nyomj entert az újrapróbáláshoz! "," | "))
+                                                input()
+                                                break
+                else:
+                        print("{:<80} {:<105} {:<3}".format(" | ",f"Név: [____________] "," | "))
+                        book.append(validateName())
+#endregion                                         
+
+
+
+#region Listings
 def reservedListings():
         banner()
         print("{:<79} {:<106} {:<3}".format(" | ","Folgalt szobákat választotta"," | "))
@@ -146,26 +295,32 @@ def roomListing():
                 print("{:<3} {:<45} {:<45} {:<45} {:<44} {:<3}".format(" | ",f"{item.roomNumber}",f"{item.bedRoom}", f"{item.equipment} / 10", f"{item.price} $", " | "))
         x = ' \c'
         print(x.replace('c',""),"-"*184, "/")
+#endregion
 
 #region Main
 def Main():
         banner()
         factory()
+        
         while True:
-                selected = int(input())
-                
-                if selected == 1:
-                        print("Szoba foglalást választotta")
-                elif selected == 2:
-                        print("Szoba foglalás lemondását választotta")   
-                elif selected == 3:
-                        roomListing()
-                elif selected == 4:
-                        reservedListings()
-                elif selected == 0:
-                        break
-                else:
-                        print("Nem megfelelő formátum! \n")
+                try:
+                        selected = int(input())
+                        if selected == 1:
+                                print("Szoba foglalást választotta")
+                                booking()
+                        elif selected == 2:
+                                print("Szoba foglalás lemondását választotta")   
+                        elif selected == 3:
+                                roomListing()
+                        elif selected == 4:
+                                reservedListings()
+                        elif selected == 0:
+                                break
+                        else:
+                                print("Nem megfelelő formátum! \n")
+                except:
+                        pass
 #endregion 
+
 
 Main()
